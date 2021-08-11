0edb16abcc905dfd9c9744a751c1fcec1f2926c0
erickxlves <alves.erick2000@hotmail.com>
2021-08-11 19:09:37 -0300

    first commit

---
+++ confere/frontend/src/Components/Navbar.js
@@ -0,0 +1,43 @@
+import './Navbar.css'
+import {Link} from 'react-router-dom'
+import { useSelector} from 'react-redux'
+
+const Navbar = ({click}) => {
+
+    const cart = useSelector((state) => state.cart)
+    const {cartItems } = cart
+
+    const getCartCount = () => {
+        return cartItems.reduce((qty, item) => qty + Number(item.qty), 0)
+    }
+    return (
+        <nav className='navbar'>
+        <div className='navbar__logo'>
+            <h2>Confere</h2>
+        </div>
+
+        <ul className='navbar__links'>
+            <li>
+                <Link to='/cart' className='cart__link'>
+                    <i className='fas fa-shopping-cart'></i>
+                     <span>
+                     Carrinho
+                    <span className='cartlogo__badge'>{getCartCount()}</span>
+                    </span>
+                </Link>
+            </li>
+            <li>
+                <Link to='/'>Inicio</Link>
+            </li>
+        </ul>
+
+        <div className='hamburger__menu' onClick={click}>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar
